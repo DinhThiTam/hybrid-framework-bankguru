cf7b584961e6752982704587e5f65bb5bec38c15
DinhThiTam <dttam.cntt@gmail.com>
2021-07-23 17:59:09 +0700

    Update_HomePageUI

--- interfaces/pageUIs/nopCommerce/HomePageUI.java
+++ interfaces/pageUIs/nopCommerce/HomePageUI.java
@@ -3,7 +3,7 @@ package pageUIs.nopCommerce;
 public class HomePageUI {
 	public static final String HOMEPAGE_SLIDER = "//div[@id='nivo-slider']";
 	public static final String REGISTER_LINK = "//a[@class = 'ico-register']";
-	public static final String GENDER_BUTTON = "//input[@id='gender-male']";
+	public static final String GENDER_MALE_RADIO = "//input[@id='gender-male']";
 	public static final String FIRSTNAME_TEXTBOX = "//input[@id='FirstName']";
 	public static final String LASTNAME_TEXTBOX = "//input[@id='LastName']";
 	public static final String EMAIL_TEXTBOX = "//input[@id='Email']";
