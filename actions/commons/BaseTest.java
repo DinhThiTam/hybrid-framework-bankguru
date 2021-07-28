20db78f2114784f5bf2cca1b2016afc396e1896c
Dinh Tam <dttam.cntt@gmail.com>
2021-07-28 18:13:00 +0700

    Update_BaseTest

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,54 @@
 package commons;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseTest {
+	private WebDriver driver;
+	String projectLocation = System.getProperty("user.dir");
+	
+	public WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectLocation + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			System.out.println("Driver init at BaseTest" + driver.toString());
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectLocation + "/browserDrivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", projectLocation + "/browserDrivers/msedgedriver.exe");
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please enter browser correct");
+		}
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		
+		return driver;
+	}
+	
+	public WebDriver getBrowserDriver(String browserName, String appURL) {
+		if (browserName.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectLocation + "\\browserDrivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+			System.out.println("Driver init at BaseTest" + driver.toString());
+		} else if (browserName.equalsIgnoreCase("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectLocation + "/browserDrivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			System.setProperty("webdriver.edge.driver", projectLocation + "/browserDrivers/msedgedriver.exe");
+			driver = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Please enter browser correct");
+		}
+		
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get(appURL);
+		return driver;
+	}
 
 }
--- testcases/com/liveguru/login/Level_04_Login_Page_Object.java
+++ testcases/com/liveguru/login/Level_04_Login_Page_Object.java
@@ -1,32 +1,32 @@
 package com.liveguru.login;
 
 import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import commons.BasePage;
+import commons.BaseTest;
 import pageObject.liveGuru.HomePageObject;
 import pageObject.liveGuru.LoginPageObject;
 import pageObject.liveGuru.MyDashboardPageObject;
 
-public class Level_04_Login_Page_Object extends BasePage {
+public class Level_04_Login_Page_Object extends BaseTest{
 	WebDriver driver;
-	String projectLocation = System.getProperty("user.dir");
 	BasePage basePage;
-	
-	
-	@BeforeClass
-	public void initBrowser() {
-	System.setProperty("webdriver.gecko.driver", projectLocation + "\\browserDrivers\\geckodriver.exe");
-	driver = new FirefoxDriver();
-	driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
+	@Parameters({"browser","url"})
+	@BeforeClass
+	public void initBrowser(String browserName, String appURL) {
+		driver = getBrowserDriver(browserName, appURL);
+		driver = new FirefoxDriver();
+		driver.get("http://live.demoguru99.com/");
 	}
+
 	@Test
 	public void Login_01_Empty_Email() {
 		driver.get("http://live.demoguru99.com/");
@@ -38,9 +38,9 @@ public class Level_04_Login_Page_Object extends BasePage {
 //		loginPage.clickToLoginButton();
 		loginPage.loginToSystem("", "");
 		Assert.assertEquals(loginPage.getEmptyEmailErrorMessage(), "This is a required field.");
-		Assert.assertEquals(loginPage.getEmptyPasswordErrorMessage(), "This is a required field.");	
+		Assert.assertEquals(loginPage.getEmptyPasswordErrorMessage(), "This is a required field.");
 	}
-	
+
 	@Test
 	public void Login_02_Invalid_Email() {
 		loginPage.refreshPage(driver);
@@ -48,9 +48,10 @@ public class Level_04_Login_Page_Object extends BasePage {
 //		loginPage.enterToPasswordTextbox("123456");
 //		loginPage.clickToLoginButton();
 		loginPage.loginToSystem("123@345.789", "123456");
-		Assert.assertEquals(loginPage.getInvalidEmailErrorMessage(), "Please enter a valid email address. For example johndoe@domain.com.");		
+		Assert.assertEquals(loginPage.getInvalidEmailErrorMessage(),
+				"Please enter a valid email address. For example johndoe@domain.com.");
 	}
-	
+
 	@Test(description = "Password less than 6 chars")
 	public void Login_03_Invalid_Password() {
 		loginPage.refreshPage(driver);
@@ -58,9 +59,10 @@ public class Level_04_Login_Page_Object extends BasePage {
 //		loginPage.enterToPasswordTextbox("123");
 //		loginPage.clickToLoginButton();
 		loginPage.loginToSystem("dam@gmail.com", "123");
-		Assert.assertEquals(loginPage.getInvalidPasswordErrorMessage(), "Please enter 6 or more characters without leading or trailing spaces.");		
+		Assert.assertEquals(loginPage.getInvalidPasswordErrorMessage(),
+				"Please enter 6 or more characters without leading or trailing spaces.");
 	}
-	
+
 	@Test(description = "Email not exist in system")
 	public void Login_04_Incorrect_Email() {
 		loginPage.refreshPage(driver);
@@ -68,9 +70,9 @@ public class Level_04_Login_Page_Object extends BasePage {
 //		loginPage.enterToPasswordTextbox("123456");
 //		loginPage.clickToLoginButton();
 		loginPage.loginToSystem(getRandomEmail(), "123456");
-		Assert.assertEquals(loginPage.getInvalidEmailOrPasswordErrorMessage(), "Invalid login or password.");		
+		Assert.assertEquals(loginPage.getInvalidEmailOrPasswordErrorMessage(), "Invalid login or password.");
 	}
-	
+
 	@Test
 	public void Login_05_Incorrect_Password() {
 		loginPage.refreshPage(driver);
@@ -78,9 +80,9 @@ public class Level_04_Login_Page_Object extends BasePage {
 //		loginPage.enterToPasswordTextbox("112233");
 //		loginPage.clickToLoginButton();
 		loginPage.loginToSystem("dam@gmail.com", "112233");
-		Assert.assertEquals(loginPage.getInvalidEmailOrPasswordErrorMessage(), "Invalid login or password.");		
+		Assert.assertEquals(loginPage.getInvalidEmailOrPasswordErrorMessage(), "Invalid login or password.");
 	}
-	
+
 	@Test
 	public void Login_06_Valid_Email_And_Password() {
 		loginPage.refreshPage(driver);
@@ -91,19 +93,20 @@ public class Level_04_Login_Page_Object extends BasePage {
 		myDashboardPage = new MyDashboardPageObject(driver);
 		Assert.assertTrue(myDashboardPage.isMyDashboardHeaderDisplay());
 	}
-	
+
 	@AfterClass
 	public void cleanBrowser() {
 		driver.quit();
 	}
-	
+
 	public String getRandomEmail() {
 		Random rand = new Random();
 		rand.nextInt(99999);
 		return "testing" + rand.nextInt(99999) + "@email.com";
 	}
+
 	HomePageObject homePage;
 	LoginPageObject loginPage;
 	MyDashboardPageObject myDashboardPage;
-	
+
 }
