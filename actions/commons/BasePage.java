0746866178938b3e7069db98e5b5717c30c919d3
Dinh Tam <dttam.cntt@gmail.com>
2021-08-11 17:56:47 +0700

    Dynamic_Locator_DataTable

--- actions/commons/BasePage.java
+++ actions/commons/BasePage.java
@@ -173,6 +173,10 @@ public class BasePage {
 		return getElements(driver, locator).size();
 	}
 	
+	public int getSizeElements(WebDriver driver, String locator, String...params) {
+		return getElements(driver, getDynamicLocator(locator, params)).size();
+	}
+	
 	public void selectDropdownByText(WebDriver driver, String locator, String itemText) {
 		select = new Select(getElement(driver, locator));
 		select.selectByVisibleText(itemText);
@@ -286,8 +290,12 @@ public class BasePage {
 	
 	public void pressKeyToElement(WebDriver driver, String locator, Keys key) {
 		action = new Actions(driver);
-		action.sendKeys(getElement(driver, locator), key).perform();
-		
+		action.sendKeys(getElement(driver, locator), key).perform();	
+	}
+	public void pressKeyToElement(WebDriver driver, String locator, Keys key, String...params) {
+		action = new Actions(driver);
+		locator = getDynamicLocator(locator, params);
+		action.sendKeys(getElement(driver, locator), key).perform();	
 	}
 	
 	public Object executeForBrowser(WebDriver driver, String javaScript) {
