5b440e131f583b91ed1559807aeec328791e34da
DT TAM <DT TAM@DESKTOP-EIC3NR5>
2021-09-17 09:21:24 -0700

    Sort_FakeData_ManageData

---
+++ testdata/com/hrm/data/Employee.java
@@ -0,0 +1,18 @@
+package com.hrm.data;
+
+public class Employee {
+	
+	public class PersonalDetail {
+		public static final String FIRST_NAME = "Dinh";
+		public static final String LAST_NAME = "Tam";
+	}
+	
+	public class ContactDetail {
+		
+	}
+	
+	public class Job {
+		
+	}
+	
+}
